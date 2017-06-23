0881cb0d1b8208452f15ad77f7ceb9a887e65017
Dvoryankin Dmitry <pchellovod@gmail.com>
2017-06-23 10:36:05 +0500

    feature: создан шаблон приложения на bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,7 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//= require jquery2
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
