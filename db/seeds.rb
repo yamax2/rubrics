22d824d8f6cf9bcc64b63e358773e42587018dda
Dvoryankin Dmitry <pchellovod@gmail.com>
2017-07-04 12:34:09 +0500

    fix: убрал фигурные скобки
    https://jira.railsc.ru/browse/SERVICES-1904

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ def random_string(length = 8)
   rand(32**length).to_s(32)
 end
 
-20.times { Rubric.create(title: "#{random_string(8)}", content: "#{random_string(8)}") }
+20.times { Rubric.create(title: random_string(8), content: random_string(8)) }
 
 500.times do
-  Rubric.create(title: "#{random_string(8)}", content: "#{random_string(8)}", rubric: Rubric.order('random()').first)
+  Rubric.create(title: random_string(8), content: random_string(8), rubric: Rubric.order('random()').first)
 end
